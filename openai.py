a4efdef7f2b1997aa34ef3bda6906727d5bd13d1
Taj Noah (AlAgawani) <33578536+tajalagawani@users.noreply.github.com>
2023-11-25 18:45:39 +0100

    Update openai.py

--- openai.py
+++ openai.py
@@ -4,68 +4,67 @@ import numpy as np
 import random
 import openai
 
-# Define your OpenAI API key (preferably as an environment variable)
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# Q-Learning settings
-n_states = 10  # Number of different types of questions
+n_states = 10  # Number  of questions
 n_actions = 5  # Number of answers to choose from for each question
 learning_rate = 0.1
 discount_factor = 0.9
 epsilon = 0.1  # Exploration rate
-n_episodes = 1000  # Number of training rounds
+n_episodes = 101# Number of training rounds
 
 # Initialize Q-table
 Q = np.zeros((n_states, n_actions))
 
-# Define function to choose the next action
+# Define the function to choose the next action
 def choose_action(state):
     if random.uniform(0, 1) < epsilon:
         action = random.randint(0, n_actions - 1)
-        print(f"[Exploration] Choosing random action {action} for state {state}")
+        print(f"Choosing random action: {action} for state: {state}")
     else:
         action = np.argmax(Q[state, :])
-        print(f"[Exploitation] Choosing best action {action} for state {state}")
+        print(f"Choosing best action: {action} for state: {state}")
     return action
 
-# Define function to update the Q-table
 def update_Q(state, action, reward):
     predict = Q[state, action]
     target = reward + discount_factor * np.max(Q[state, :])
     Q[state, action] += learning_rate * (target - predict)
-    print(f"Updated Q-table at state {state}, action {action} with reward {reward}")
+    print(f"Updated Q-table at state: {state}, action: {action}, reward: {reward}")
 
-# Function to interact with the GPT model via the chat API
+# Function to interact with GPT-4 (or the appropriate model) via the chat API
 def get_LLM_answers(question):
     print(f"Fetching answers for the question: '{question}'")
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-4",  # Replace with the appropriate model
+            model="gpt-4",  # Ensure this is a valid model identifier
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": question}
             ]
         )
-        print(f"Full response from OpenAI: {response}")
-        if 'error' in response:
-            print(f"Error from OpenAI: {response['error']['message']}")
-            return None
+        
+        # Extracting the response correctly
+        if 'choices' in response and len(response['choices']) > 0:
+            message_content = response['choices'][0]['message']['content']
+            print(f"Received answer: {message_content}")
+            return [message_content]
         else:
-            answers = [msg['content'] for msg in response['choices'][0]['message'] if msg['role'] == 'assistant']
-            print(f"Received answers: {answers}")
-            return answers
+            print("No valid response in 'choices'")
+            return None
     except openai.error.OpenAIError as e:
-        print(f"OpenAI API error occurred: {e}")
+        print(f"errrrrrrrorrr: {e}")
         return None
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
         return None
 
+
 # Function for evaluating the chosen answer
 def evaluate_answer(answer, question):
-    # Modify this logic based on how you want to evaluate the answer
+    # Implement the logic to evaluate the answer
+    # Example: Check if the answer is non-empty or contains certain keywords
     reward = 1 if answer and 'correct' in answer else 0
-    print(f"Evaluating answer '{answer}' for question '{question}', Reward: {reward}")
     return reward
 
 # Function to generate or fetch a new question
@@ -79,28 +78,24 @@ def generate_question():
     ]
     question = random.choice(questions)
     state = questions.index(question) % n_states
-    print(f"Generated question: '{question}', State: {state}")
     return question, state
 
 # Main Q-learning loop
 for episode in range(n_episodes):
-    print(f"\nStarting episode {episode + 1}")
     question, state = generate_question()
     answers = get_LLM_answers(question)
     
     if not answers:
-        print("No answers received, skipping this episode.")
-        continue
+        continue  # Skip to the next episode if no answers were received
 
     action = choose_action(state)
     if action >= len(answers):
-        print(f"Action {action} is out of range. Adjusting to fit the available answers.")
-        action = len(answers) - 1
+        action = len(answers) - 1  # Adjust if action is out of range
 
     chosen_answer = answers[action]
     reward = evaluate_answer(chosen_answer, question)
     update_Q(state, action, reward)
 
 # Output the final Q-table
-print("\nTraining completed. Final Q-table:")
+print("Training completed. Final Q-table:")
 print(Q)
